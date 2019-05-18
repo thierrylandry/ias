592609f742e18e8bb54891368dd5ea8bacf0d675
Wilfried KOFFI <glamolondon@gmail.com>
2019-05-18 11:28:14 +0000

    feat:change location

--- resources/views/pdf/layout.blade.php
+++ resources/views/pdf/layout.blade.php
@@ -37,7 +37,7 @@
 </main>
 
 <footer>
-    <p>Situté à Angré 8ème tranche</p>
+    <p>Situé à Angré 8ème tranche</p>
     <p>Tel : +225 24 00 25 54 / +225 07 93 97 12 / +225 06 72 68 83 - 13 BP 1715 Abidjan 13</p>
     <p>N° CC 0526299 H Réel Simplifié d'Imposition Centre des Impôts d'Abobo III N° RC : CI-ABJ-2008-A-1483</p>
     <p>IBAN : CI93 CI04 2012 - Compte Bancaire BIAO N° 03536196357524  - SWIFT Code : BIAOCIABXXX</p>
